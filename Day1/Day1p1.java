ed7a7def679a023590dc71dd3978988a981583ca
SunshineLuke90 <Sunshineluke90@gmail.com>
2023-02-08 22:46:56 -0600

    Day 1

--- Day1/Day1p1.java
+++ Day1/Day1p1.java
@@ -11,14 +11,13 @@ public class Day1p1 {
         //initialize file object
         File file = new File("Day1\\Day1p1.txt");
         // Print each row in file by line
-        System.out.println(findMax(file));
-        
+        System.out.println(findMax(ListSumsFromFile(file)));
+        System.out.println(findSumTop(ListSumsFromFile(file), 3));
     }
-    public static int findMax(File file){
+    public static ArrayList<Integer> ListSumsFromFile(File file){
         int sum = 0;
-        int max = 0;
+        ArrayList<Integer> list = new ArrayList<Integer>();
         try (Scanner scanner = new Scanner(file)) {
-            ArrayList<Integer> list = new ArrayList<Integer>();
             while (scanner.hasNextLine()) {
                 String check = scanner.nextLine();
                 if(check == ""){
@@ -29,14 +28,22 @@ public class Day1p1 {
                 int line = Integer.parseInt(check);
                 sum += line;
             }
-            //sort list
-            list.sort(Comparator.naturalOrder());
-            //find max
-            max = list.get(list.size()-1);
             scanner.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        return max;
+        return list;
+    }
+    public static int findMax(ArrayList<Integer> list){
+        list.sort(Comparator.naturalOrder());
+        return list.get(list.size()-1);
+    }
+    public static int findSumTop(ArrayList<Integer> list, int topX){
+        list.sort(Comparator.naturalOrder());
+        int sum = 0;
+        for(int i = 0; i < topX; i++){
+            sum += list.get(list.size()-1-i);
+        }
+        return sum;
     }
 }
